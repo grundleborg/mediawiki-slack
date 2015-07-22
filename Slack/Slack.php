ca960d1971a7445a7eec997f80d27aa215312fb1
Martijn Engler <git@martijnengler.nl>
2015-07-22 17:09:11 +0200

    Add hook on PageContentInsertComplete to work on new pages too

--- Slack/Slack.php
+++ Slack/Slack.php
@@ -33,6 +33,7 @@ $dirbasename = basename( $dir );
 $wgAutoloadClasses['SlackHooks'] = $dir . '/Slack.hooks.php';
 
 // Register hooks
+$wgHooks['PageContentInsertComplete'][] = 'SlackHooks::onPageContentSaveComplete';
 $wgHooks['PageContentSaveComplete'][] = 'SlackHooks::onPageContentSaveComplete';
 
 // Configuration Defaults
